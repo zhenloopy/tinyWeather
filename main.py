179768137cb9bf2c42510af33e639c67873292d8
zhenloopy <145357830+zhenloopy@users.noreply.github.com>
2024-12-15 09:15:27 -0600

    Added notification parameters

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ import subprocess
 
 def getWeatherData():
     url = "https://api.weather.gov/gridpoints/HGX/28,135/forecast"
-    r = requests.get(url, headers={"User-Agent": "(abcdeTinyWeather.1.1.0, testemail@gmail.com)"}).json()
+    r = requests.get(url, headers={"User-Agent": "(TinyWeather.1.1.0, zhenloopy@gmail.com)"}).json()
 
     todayForecast = r["properties"]["periods"][0]["detailedForecast"]
     keywords = ["storm", "rain"]
 
     if any([x in todayForecast for x in keywords]):
-        raining = subprocess.run(['notify-send', 'Weather Alert', 'It will rain today'], check=True, text=True)
-    else:
-        print("clear")
+        subprocess.run(['notify-send', '-i', '/home/zhenloopy/tinyWeather/weathericon.png', 'Weather Alert', 'It will rain today', '-u', 'critical'], check=True, text=True)
 
 
 
