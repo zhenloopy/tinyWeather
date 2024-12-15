7dff8e39ba4fa460e67667590d5f3d91329a7702
zhenloopy <145357830+zhenloopy@users.noreply.github.com>
2024-12-15 10:38:32 -0600

    Added temperature info and fixed rain logic

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import requests
-import json
 import subprocess
 
 
-def getWeatherData():
-    url = "https://api.weather.gov/gridpoints/HGX/28,135/forecast"
+def getURL():
+    ip = requests.get("http://ip-api.com/json/?fields=lat,lon").json()
+    url = requests.get("https://api.weather.gov/points/" + str(ip["lat"]) + "," + str(ip["lon"])).json()["properties"]["forecast"]
+    print(url)
+    return url
+    
+
+def getWeatherData(url):
     r = requests.get(url, headers={"User-Agent": "(TinyWeather.1.1.0, zhenloopy@gmail.com)"}).json()
 
     todayForecast = r["properties"]["periods"][0]["detailedForecast"]
+    todayPrecipChance = r["properties"]["periods"][0]["probabilityOfPrecipitation"]["value"]
+    todayLow = r["properties"]["periods"][1]["temperature"]
+    todayHigh = r["properties"]["periods"][0]["temperature"]
     keywords = ["storm", "rain"]
 
-    if any([x in todayForecast for x in keywords]):
-        subprocess.run(['notify-send', '-i', '/home/zhenloopy/tinyWeather/weathericon.png', 'Weather Alert', 'It will rain today', '-u', 'critical'], check=True, text=True)
+    message = "Low: " + str(todayLow) + ", High: " + str(todayHigh)
+    if any([x in todayForecast for x in keywords]) and todayPrecipChance != None and todayPrecipChance >= 40:
+        message += ", Rain expected"
+    
 
+    subprocess.run(['notify-send', '-i', '/home/zhenloopy/tinyWeather/weathericon.png', 'Weather Alert', message, '-u', 'critical'], check=True, text=True)
 
-
-getWeatherData()
+url = getURL()
+getWeatherData(url)
